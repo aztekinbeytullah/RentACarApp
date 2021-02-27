e02d15996b1ebd3528b8588d0cca6e33b3a311da
Beytullah AZTEKİN <aztekinbeytullah@gmail.com>
2021-02-27 11:18:59 +0300

    ...

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,13 +1,30 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, RentacarContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            using (RentacarContext context=new RentacarContext())
+            {
+                var result= from rental in context.Rentals
+                            join car in context.Cars
+                            on rental.CarId equals car.Id
+
+                            join 
+
+            }
+
+
+            return null;
+        }
+
     }
 }
